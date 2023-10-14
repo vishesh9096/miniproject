f8e46e2079ceb21186f579e3a7759025dfb0e281
Pankhudi <107767848+Pankhudi18@users.noreply.github.com>
2023-10-14 12:53:30 +0530

    Priya's UI

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/navbar";
 export default function Community() {
-    return(
+    return (
         <main className="min-h-screen">
             <Navbar />
             <CommunityUpper />
@@ -10,12 +10,49 @@ export default function Community() {
 };
 
 const CommunityUpper = () => {
-    return(
-        <div className="tutorial bg-gray-200 p-8">
-      <div className="tutorial-upper text-4xl font-bold mx-16 mt-4">
-        Community Forum
-      </div>
-      </div>
+    return (
+        <div className="tutorial bg-white-300 p-8">
+            <div className="tutorial-upper text-4xl font-bold mx-16 mt-4">
+                Community Forum
+                <section class="text-white-600 body-font relative">
+                    <div class="container px-5 py-24 mx-auto">
+                        {/* <div class="flex flex-col text-center w-full mb-12">
+                            {/* <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Welcome to Our Community</h1> }
+                            {/* <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>}
+                        </div> */}
+                        <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                            <div class="flex flex-wrap -m-2">
+                                <div class="p-2 w-1/2">
+                                    <div class="relative">
+                                        {/* <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/> */}
+                                    </div>
+                                </div>
+                                <div class="p-2 w-1/2">
+                                    <div class="relative">
+                                        {/* <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/> */}
+                                    </div>
+                                </div>
+                                <p class="mt-1">Add your msg to the Community</p>
+                                <br/>
+                                <div class="p-2 w-full">
+                                    <div class="relative">
+                                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                                        <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    </div>
+                                </div>
+                                <div class="p-2 w-full">
+                                    <button class="flex mx-auto text-white bg-gray-600 border-white py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg">Submitt</button>
+                                    {/* <button class="flex mx-auto bg-gray-600 text-white border-white hover:bg-orange-600">Button</button> */}
+                                
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
     );
 };
-
--- src/app/tip/page.js
+++ src/app/tip/page.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/navbar";
 export default function Tip() {
-    return(
+    return (
         <main className="min-h-screen">
             <Navbar />
             <TipUpper />
@@ -10,12 +10,97 @@ export default function Tip() {
 };
 
 const TipUpper = () => {
-    return(
+    return (
         <div className="tutorial bg-gray-200 p-8">
-      <div className="tutorial-upper text-4xl font-bold mx-16 mt-4">
-        Tips
-      </div>
-      </div>
+            <div className="tutorial-upper text-4xl font-bold mx-16 mt-4">
+                Tip of the day
+                <section class="text-gray-600 body-font">
+                    <div class="container px-5 py-24 mx-auto">
+                        <div class="flex flex-wrap -m-4">
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/premium-vector/funny-chubby-girl-lying-her-side-it-says-love-your-body-your-hair-beautiful-girl-laughs-broadcasts-body-positive_637281-74.jpg?w=2000"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">FITNESS</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">You are Beautiful</h2>
+                                    <p class="mt-1 transition: transform 1s;">Take care of your body.</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/free-vector/personalized-workouts-concept-illustration_114360-7281.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">FITNESS</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">You can do it!!!</h2>
+                                    <p class="mt-1">All progress will take place.</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/free-vector/eating-disorder-concept-illustration_114360-8026.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">FITNESS</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">All change are for a reason </h2>
+                                    <p class="mt-1">You can do it </p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://static.vecteezy.com/system/resources/thumbnails/007/885/678/small_2x/flat-isometric-illustration-concept-exercise-plan-for-weight-loss-diet-free-vector.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">FITNESS</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">Towards New You</h2>
+                                    <p class="mt-1">The real workout has started.</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/premium-vector/smiling-woman-healthy-food-oncept-healthy-eating-vector-illustration_263366-640.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">DIET</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">Let's eat healthy</h2>
+                                    <p class="mt-1">Let body acheive</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/premium-vector/stress-dieting-weight-loss-concept_140689-2364.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">DIET</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">DIET ain't Dieting</h2>
+                                    <p class="mt-1">Eat intelligently.</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://img.freepik.com/free-vector/diet-plan-schedule_3446-617.jpg"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">DIET</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">Healthy Diet is Must</h2>
+                                    <p class="mt-1">Eat intelligently.</p>
+                                </div>
+                            </div>
+                            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                <a class="block relative h-48 rounded overflow-hidden">
+                                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://us.123rf.com/450wm/alenast/alenast1807/alenast180700248/114770521-cheat-day-poster-card-with-line-art-hamburger-cup-and-fries-ice-cream-clean-design.jpg?ver=6"/>
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">DIET</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">Cheat meal time!!!</h2>
+                                    <p class="mt-1">Eat intelligently.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
     );
 };
-
